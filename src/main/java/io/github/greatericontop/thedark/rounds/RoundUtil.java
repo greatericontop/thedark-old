b51aa1fd6be5313c38d7d0b392ea3675de4952ff
greatericonrop <greateric@protonmail.com>
2023-08-31 11:33:50 -0400

    a bit of input validation

--- src/main/java/io/github/greatericontop/thedark/rounds/RoundUtil.java
+++ src/main/java/io/github/greatericontop/thedark/rounds/RoundUtil.java
@@ -4,7 +4,9 @@ import io.github.greatericontop.thedark.TheDark;
 import io.github.greatericontop.thedark.rounds.data.RoundData;
 import io.github.greatericontop.thedark.rounds.operation.BaseOperation;
 import io.github.greatericontop.thedark.rounds.operation.OperationContext;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +20,30 @@ public class RoundUtil {
     }
     public static void executeRound(TheDark plugin, int roundNumber) {
         List<Location> locations = new ArrayList<>();
-        for (Object o : plugin.getConfig().getList("valid-spawn-locations")) {
-            List<Object> ol = (List<Object>) o; // hope that the input is valid...
+        List validSpawnLocations = plugin.getConfig().getList("valid-spawn-locations");
+        if (validSpawnLocations == null) {
+            plugin.getLogger().warning(":valid-spawn-locations: is null! (can't execute round)");
+            return;
+        }
+        for (Object o : validSpawnLocations) {
+            if (!(o instanceof List locationObject)) {
+                plugin.getLogger().warning("Values in :valid-spawn-locations: must be lists! (skipping)");
+                continue;
+            }
+            if (locationObject.size() != 4) {
+                plugin.getLogger().warning("Values in :valid-spawn-locations: must have exactly 4 elements! (skipping)");
+                continue;
+            }
+            String worldName = (String) locationObject.get(0);
+            World world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                plugin.getLogger().warning("World " + worldName + " does not exist! (skipping)");
+                continue;
+            }
+            double x = (double) locationObject.get(1);
+            double y = (double) locationObject.get(2);
+            double z = (double) locationObject.get(3);
+            locations.add(new Location(world, x, y, z));
         }
         //OperationContext ctx = new OperationContext(plugin);
         //executeRound(ctx, roundNumber);
